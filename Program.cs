05816b1d1a524aeea12dd4201397dda1fc735602
ParkMinsoo <harimaho88@gmail.com>
2025-04-15 20:08:58 +0900

    asdf

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TextRPG
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string job = "";
+            
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("플레이할 캐릭터의 이름을 적어 주세요.\n");
+            
+            string playerName = Console.ReadLine();
+            
+            Console.WriteLine(" ");
+            Console.WriteLine("당신의 직업은 무엇입니까?");
+            Console.WriteLine("1. 전사 \n2. 도적 \n3. 궁수 \n4. 마법사");
+
+            int num = int.Parse(Console.ReadLine());
+            
+            if (num ==1)
+            {
+               job = "전사";
+            }
+            else if (num == 2)
+            {
+                job = "도적";
+            }
+            else if (num == 3)
+            {
+                job = "궁수";
+            }
+            else if (num == 4)
+            {
+                job = "마법사";
+            }
+            else
+            {
+                
+            }
+            Player player = new Player();
+            player.job = job;
+            player.playerName = playerName;
+
+            Console.WriteLine(" ");
+            Console.WriteLine($"환영합니다. 전설의 {player.job} {player.playerName}님.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다. \n \n1. 상태 보기 \n2. 인벤토리 \n3. 상점 \n");
+            SelectMenu(player);
+        }
+
+        static void SelectMenu(Player player)
+        {
+
+            Console.Write("원하시는 행동을 입력해주세요. ");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("상태 보기");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+
+                player.PlayerStatus();
+
+                if (num == 0)
+                {
+
+                }
+            }
+            if (num == 2)
+            {
+                ShowInventory();
+            }
+            if (num == 3)
+            {
+                Store();
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다. 다시 선택해 주세요.");
+            }
+        }
+
+        class Player()
+        {
+            public int level = 1;
+            public string playerName ;
+            public string job;
+            public int attackpower;
+            public int defense;
+            public int health = 50;
+            public int gold = 1500;
+
+            public void PlayerStatus()
+            {
+                Console.WriteLine("Lv. " + level);
+                Console.WriteLine("이름 : " + playerName);
+                Console.WriteLine("직업 : " + job);
+                Console.WriteLine("공격력 : " + attackpower);
+                Console.WriteLine("방어력 : " + defense);
+                Console.WriteLine("체 력 : " + health);
+                Console.WriteLine("Gold : " + gold + "G");
+            }
+         }
+        
+        static void ShowInventory()
+        {
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+        }
+
+        static void Store()
+        {
+
+        }
+    }
+}
